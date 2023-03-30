7433d4b22262c71579e8ef01c305b13f754b8f40
devincy@gmail.com <lathfulbert@gmail.com>
2023-03-30 18:12:08 +0000

    creation de seeder

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Etudiant extends Model
 {
     use HasFactory;
+
+    protected $fillable =  ["nom", "prenom", "sexe", "age", "niveau_scolaire_id"];
+
+    public function niveau_scolaire(){
+
+        return $this->belongTo(NiveauScolaire::class);
+
+    }
 }
--- app/Models/NiveauScolaire.php
+++ app/Models/NiveauScolaire.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class NiveauScolaire extends Model
 {
     use HasFactory;
+
+    protected $fillable =  ["nom"];
 }
