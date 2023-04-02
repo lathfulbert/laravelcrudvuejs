3f20e519dfa3ea11f952c5c991ed3a8d9887a98a
devincy@gmail.com <lathfulbert@gmail.com>
2023-04-02 14:41:52 +0000

    final pagination et debut Add niveau

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,12 +1,12 @@
 <template>
      <ul class="pagination pagination-sm float-right">
-                     <li class="page-item"><Link class="page-link" :href="props.prev">«</Link></li>
+                     <li class="page-item"  :class="{ 'disabled' : props.prev == null}"><Link class="page-link" :href="props.prev">«</Link></li>
                   
-                    <li class="page-item" v-for="link in computedLinks">
+                    <li class="page-item" :class="{ 'active' : link.active}" v-for="link in computedLinks">
                         <Link class="page-link" :href="link.url">{{ link.label }}</Link>
                     </li>
 
-                    <li class="page-item"><Link class="page-link" :href="props.next">»</Link></li>
+                    <li class="page-item" :class="{ 'disabled' : props.next == null}"><Link class="page-link" :href="props.next">»</Link></li>
       </ul>
 
 
